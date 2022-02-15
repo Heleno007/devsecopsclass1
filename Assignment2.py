aa543c971aea73ab5ed774aea1a8ebdac117f124
Ubuntu <ubuntu@ip-172-31-31-254.ec2.internal>
2022-02-15 21:44:27 +0000

    Assignment2 added

---
+++ Assignment2.py
@@ -0,0 +1,14 @@
+#What is the command to clear your terminal?
+# Ans= clear
+
+#What is the command to delete a file?
+# Ans= rm <filename>
+
+#What command is used to change directories?
+# Ans= cd <dir> or cd ..
+
+#What command is issued to get the ip address of all interfaces on a server?
+# Ans= 'ifconfig'
+
+#What command is used to change the ownership of a file?
+# Ans= 'chown'
