bbad0a54ce1d86be4d64bc0d652752b5bbd31aea
Ubuntu <ubuntu@ip-172-31-31-254.ec2.internal>
2022-02-16 19:07:09 +0000

    Assignment3.py added

---
+++ Assignment3.py
@@ -0,0 +1,23 @@
+# Wednesday February 16th
+
+#What is the default text editor for Bash shell with a Windows-based Git install?
+# Ans = vi or vim
+
+#Before you install Git, which of the following prerequisite products must be present and configured on your local OS?
+# Ans= Java Development kit 1.8 or newer.
+
+#After you install Git and prior to issuing the first commit, which two configuration properties does the toll expect to be configured?
+# Ans= git config --global user.name  "yourname"
+#      git config --global user.email "youremailaddress"
+
+#Who is attributed with the creation of git?
+# Ans= Linus Torvalds
+
+#After you initialize a new Git repository and create a file named git-quiz.html, which of the following commands will not work if issued?
+# Ans= d. git commit -m "gitquiz web file added"
+#
+#Which command should you use to intialize a new repository?
+# Ans= git init
+
+
+
